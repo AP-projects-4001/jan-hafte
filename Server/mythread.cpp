2221a98523f96190d3bc09462819f8f709e8154e
Atid <khodaaie@gmail.com>
2022-06-26 21:49:13 +0430

    server handle register & login

--- Server/mythread.cpp
+++ Server/mythread.cpp
@@ -1,7 +1,14 @@
 #include "mythread.h"
+#include <QTimer>
+#include <QFile>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonValue>
+#include <QDebug>
+#include <QFile>
 
-MyThread::MyThread(qintptr ID, QObject *parent) :
-    QThread(parent)
+MyThread::MyThread(qintptr ID, QObject *parent) : QThread(parent)
 {
     this->socketDescriptor = ID;
 }
@@ -14,7 +21,7 @@ void MyThread::run()
     socket = new QTcpSocket();
 
     // set the ID
-    if(!socket->setSocketDescriptor(this->socketDescriptor))
+    if (!socket->setSocketDescriptor(this->socketDescriptor))
     {
         // something's wrong, we just emit a signal
         emit error(socket->error());
@@ -38,21 +45,103 @@ void MyThread::run()
     exec();
 }
 
+int checkValidUsername(QString username, QByteArray jsonDb)
+{
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(jsonDb);
+    QJsonObject jsonObj = jsonDoc.object();
+    QJsonArray jsonArr = jsonObj["users"].toArray();
+    for (int i = 0; i < jsonArr.size(); i++)
+    {
+        QJsonObject user = jsonArr[i].toObject();
+        if (user["username"].toString() == username)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 void MyThread::readyRead()
 {
     // get the information
     QByteArray Data = socket->readAll();
+    QJsonDocument doc = QJsonDocument::fromJson(Data);
+    QJsonObject readData = doc.object();
+    QString header = readData["header"].toString();
+    if (header == "register")
+    {
+        QFile file("./user.json");
+        if (!file.open(QIODevice::ReadWrite))
+        {
+            qDebug() << "Error opening file";
+        }
+        QByteArray data = file.readAll();
+        if (checkValidUsername(readData["username"].toString(), data) == 0)
+        {
+            socket->write("failed");
+            return;
+        }
+        QJsonDocument jsonDoc = QJsonDocument::fromJson(data);
+        QJsonObject jsonObj = jsonDoc.object();
+        QJsonArray jsonArr = jsonObj["users"].toArray();
+        QJsonObject newUser;
+        newUser["username"] = readData["username"].toString();
+        newUser["password"] = readData["password"].toString();
+        jsonArr.append(newUser);
+        jsonObj["users"] = jsonArr;
+        QJsonDocument newDoc(jsonObj);
+        file.resize(0);
+        file.write(newDoc.toJson());
+        file.close();
+        socket->write("success");
+    }
+    else if (header == "login")
+    {
+        QFile file("./user.json");
+        if (!file.open(QIODevice::ReadOnly))
+        {
+            qDebug() << "Error opening file";
+        }
+        QByteArray data = file.readAll();
+        if (checkValidUsername(readData["username"].toString(), data) == 1)
+        {
+            socket->write("not valid");
+            return;
+        }
+        QJsonDocument mydoc = QJsonDocument::fromJson(data);
+        QJsonObject myobj = mydoc.object();
+        QJsonArray myarr = myobj["users"].toArray();
+        for (int i = 0; i < myarr.size(); i++)
+        {
+            QJsonObject user = myarr[i].toObject();
+            if (user["username"].toString() == readData["username"].toString() && user["password"].toString() == readData["password"].toString())
+            {
+                socket->write("success");
+                return;
+            }
+        }
+        socket->write("failed");
+    }
 
     // will write on server side window
     qDebug() << socketDescriptor << " Data in: " << Data;
 
-    socket->write(Data);
+    // write to socket
+    // read file data from local
+    /*QFile file("C:/test.txt");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+    QTextStream in(&file);
+    QString line = in.readLine();
+    while (!line.isNull()) {
+        socket->write(line.toUtf8());
+        line = in.readLine();
+    }*/
 }
 
 void MyThread::disconnected()
 {
     qDebug() << socketDescriptor << " Disconnected";
-
 
     socket->deleteLater();
     exit(0);
